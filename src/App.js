ef9ef7a2f7af95cde799f46d740ee71d11d7f908
Doiphode <T50482@intelizign.com>
2024-03-26 17:00:33 +0530

    Trainee sidebar added

--- src/App.js
+++ src/App.js
@@ -10,36 +10,11 @@ import Homepage from './Pages/HomePage';
 import Login from './Pages/LoginPage';
 import Logout from "./Pages/Logout";
 import Messages from "./Pages/Messages";
-<<<<<<< HEAD
 import TraineeExamDetails from "./Pages/TraineeExamDetails";
-=======
-import Order from "./Pages/Order";
-import Saved from "./Pages/Saved";
-import Setting from "./Pages/Setting";
->>>>>>> db4f17dcfba5c57cfaf712984c279ad4f09c485c
 import Users from "./Pages/Users";
 import { RequireAuth } from './authService/auth';
 import "./index.css";
 function App() {
-<<<<<<< HEAD
-=======
-  let navigate = useNavigate();
-  let auth = useAuth();
-  let handleRefresh = async () => {
-    try {
-      let response = await api.get("/api/users/");
-      let { data } = response.data;
-      console.log("Logged in after refresh.");
-      auth.setUser(data.user)
-    } catch (error) {
-      navigate("/login", { replace: true, state: "Please login first.." });
-    }
-  }
-  // useEffect(() => {
-  //   handleRefresh();
-  // })
-
->>>>>>> db4f17dcfba5c57cfaf712984c279ad4f09c485c
   return (
 
     <>
@@ -52,7 +27,6 @@ function App() {
         {/**
            * TODO: Make group of all admin routes and user routes below
           */}
-<<<<<<< HEAD
         <Route element={<RequireAuth> <MasterLayout /> </RequireAuth>}>
           <Route element={<AdminLayout />}>
 
@@ -75,17 +49,6 @@ function App() {
 
             <Route path="/logout" element={<Logout />} />
           </Route>
-=======
-        <Route element={<RequireAuth> <Layout/> </RequireAuth>}>
-          <Route path="/dashboard" element={ <Dashboard /> } />
-          <Route path="/users" element={ <Users /> } />
-          <Route path="/messages" element={<Messages /> } />
-          <Route path="/analytics" element={ <Analytics /> } />
-          <Route path="/file-manager" element={<FileManager /> } />
-          <Route path="/order" element={<Order /> } />
-          <Route path="/saved" element={<Saved /> } />
-          <Route path="/settings" element={<Setting /> } />
->>>>>>> db4f17dcfba5c57cfaf712984c279ad4f09c485c
         </Route>
       </Routes>
     </>
