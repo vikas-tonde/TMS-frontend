ad1d35e9c5b105945de4e07f14adba3cb41925c6
Panhale <T50494@intelizign.com>
2024-03-27 12:12:31 +0530

    Trainee

--- src/Components/TraineeExamData.js
+++ src/Components/TraineeExamData.js
@@ -104,12 +104,9 @@ export default function TraineeExamData() {
             </div>
           </div>
           <div className=" flex items-center justify-center ">
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Submit
-            </button>
+          <button className=" px-12 py-3 bg-[#0A1C3E] text-xl text-white font-semibold drop-shadow-lg rounded-full items-center justify-center" type='submit' >
+                          Submit
+          </button>
           </div>
           </div>
       
