ca6959771bfccdf78cc4333cf4aabccb7674cc72
rutik <rutik@DESKTOP-L8D5RG0>
2024-03-30 15:44:45 +0530

    Batch fetching batchname

--- src/Pages/Batch.jsx
+++ src/Pages/Batch.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const Batch = () => {
-   
+  const [batch, setBatch] = useState(null);
     useEffect (()=> {
         axios.get('/api/admin/batches')
-        .then(res=> console.log(res.data.data[0].trainees))
+        .then(res=> setBatch(res.data.data[0].batchName))
         .catch(console.log("Error in fetching "));
     }, [])
 
@@ -13,8 +13,7 @@ const Batch = () => {
 
         <>
           <div className="flex-1 m-10" >
-          <h1 className="pt-5 pl-5 pr-10 font-extrabold text-3xl font-serif">This is Batch Details</h1>
-
+          <h1 className="pt-5 pl-5 pr-10 font-extrabold text-3xl font-serif">{batch}</h1>
           </div>
         </>
       );
