b50df4f7bd75fa2f3f9cbea148b84bd530ee7986
Vikas Tonde <64477365+vikas-tonde@users.noreply.github.com>
2024-12-09 12:39:36 +0530

    Deleted older faulty files

--- src/Components/Layouts/MasterLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-function MasterLayout() {
-    return <Outlet></Outlet>;
-}
-
-export default MasterLayout
--- src/Pages/Analytics.jsx
+++
@@ -1,5 +0,0 @@
-const Analytics = () => {
-  return <h1 className="pt-5 pl-5 pr-10 font-extrabold text-3xl font-serif">Marks Statistics</h1>
-};
-
-export default Analytics;
